e6a96a9edc08690e126f46765bc31205e5f014b9
Brandon Schmeidler <brandon.bsgstudio@gmail.com>
2021-02-10 12:52:58 -0600

    What an absolute mess this is right now.

--- vJoyMidiFeeder/vJoyMidiFeeder/src/main.cpp
+++ vJoyMidiFeeder/vJoyMidiFeeder/src/main.cpp
@@ -1,218 +1,466 @@
-#include "AppHeaders.h"
-#include <unordered_map>
+#include <string>
+#include <vector>
 
-enum eAxis {
-	X, Y, Z, RX, RY, RZ, SL0, SL1
-};
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
-struct MIDI_CC {
-	int channel;
-	int id;
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
-	MIDI_CC() {
-		channel = 1;
-		id = 0;
-	}
+#pragma region ImGui Functions
+void imgui_new_frame() {
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+}
+#pragma endregion
 
-	MIDI_CC(int midi_channel, int cc) {
-		channel = midi_channel;
-		id = cc;
-	}
-};
+#pragma region GLFW Functions
+static void glfw_framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+#pragma endregion
 
-struct VJOYMIDI_BINDING {
-	int device_id;
-	MIDI_CC axes[8];
-};
+#pragma region Application Variables
+const std::string APP_TITLE = "vJoy MIDI Feeder";
+const std::string APP_VERSION = "0.0.0";
+const char* GLSL_VERSION = "#version 440";
+#pragma endregion
 
-int vjoy_device_id = 1;
-int midi_device_id = 0;
-const int MIDI_MSG_CC = 176;
-const long max_value = 0x8000;
 
-BOOL used_axis_cache[8];
+#pragma region Application GUI
 
-JOYSTICK_POSITION_V2 iReport;
-VJOYMIDI_BINDING binding;
-eAxis axis_bind_type = eAxis::X;
+void gui_dock_initial_layout(GLFWwindow* window, ImGuiID dockspace_id) {
+	static bool ran_already = false;
 
-MIDI_CC last_midi_message;
+	if (ran_already) return;
 
-void cache_used_axes() {
-	used_axis_cache[eAxis::X] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_X);
-	used_axis_cache[eAxis::Y] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_Y);
-	used_axis_cache[eAxis::Z] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_Z);
-	used_axis_cache[eAxis::RX] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_RX);
-	used_axis_cache[eAxis::RY] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_RY);
-	used_axis_cache[eAxis::RZ] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_RZ);
-	used_axis_cache[eAxis::SL0] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_SL0);
-	used_axis_cache[eAxis::SL1] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_SL1);
+	int width, height;
+	glfwGetWindowSize(window, &width, &height);
+
+	ImGui::DockBuilderRemoveNode(dockspace_id);
+	ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
+	ImGui::DockBuilderSetNodeSize(dockspace_id, ImVec2(width, height));
+
+	ImGuiID dock_main_id = dockspace_id;
+	
+	ImGuiID dock_id_prop_top = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Left, 0.20f, nullptr, &dock_main_id);
+	//ImGuiID dock_id_prop_bottom = ImGui::DockBuilderSplitNode(dock_id_prop_top, ImGuiDir_Down, 0.5f, nullptr, &dock_id_prop_top);
+	ImGuiID dock_id_bottom = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Down, 0.20f, nullptr, &dock_main_id);
+	
+	ImGuiDockNode* dock_node = ImGui::DockBuilderGetNode(dock_id_prop_top);
+	dock_node->LocalFlags |= ImGuiDockNodeFlags_NoWindowMenuButton | ImGuiDockNodeFlags_NoCloseButton;
+
+	//dock_node = ImGui::DockBuilderGetNode(dock_id_prop_bottom);
+	//dock_node->LocalFlags |= ImGuiDockNodeFlags_NoWindowMenuButton | ImGuiDockNodeFlags_NoCloseButton;
+
+	dock_node = ImGui::DockBuilderGetNode(dock_id_bottom);
+	dock_node->LocalFlags |= ImGuiDockNodeFlags_NoWindowMenuButton | ImGuiDockNodeFlags_NoCloseButton;
+
+	dock_node = ImGui::DockBuilderGetNode(dock_main_id);
+	dock_node->LocalFlags |= ImGuiDockNodeFlags_NoWindowMenuButton | ImGuiDockNodeFlags_NoCloseButton | ImGuiDockNodeFlags_NoTabBar;
+
+	ImGui::DockBuilderDockWindow("Device Manager", dock_id_prop_top);
+	//ImGui::DockBuilderDockWindow("MIDI Devices", dock_id_prop_top);
+	//ImGui::DockBuilderDockWindow("vJoy Devices", dock_id_prop_bottom);
+	ImGui::DockBuilderDockWindow("MIDI Monitor", dock_id_bottom);
+	ImGui::DockBuilderDockWindow("Input Binder", dock_main_id);
+	ImGui::DockBuilderFinish(dockspace_id);
+	ran_already = true;
 }
 
-void midi_bind_callback(double deltatime, std::vector<unsigned char>* message, void* userData) {
-	int status = (int)message->at(0);
-	if (status >= MIDI_MSG_CC && status < MIDI_MSG_CC + 16) {
-		int channel = status - MIDI_MSG_CC + 1;
-		int cc = (int)message->at(1);
-
-		MIDI_CC axis = binding.axes[axis_bind_type];
-		axis.channel = channel;
-		axis.id = cc;
+void gui_input_binder() {
+	ImGui::Begin("Input Binder", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
+	for (int i = 0; i < 10; ++i) {
+		ImGui::Text("Bindy %d", i);
 	}
+	ImGui::End();
 }
 
-void midi_monitor_callback(double deltatime, std::vector< unsigned char > *message, void *userData)
-{
-	int status = (int)message->at(0);
-	if (status >= MIDI_MSG_CC && status < MIDI_MSG_CC + 16) {
-		int channel = status - MIDI_MSG_CC + 1;
-		int cc = (int)message->at(1);
-		int val = (int)message->at(2);
+void gui_devices_midi() {
+	std::vector<std::string> fake_midi_devices;
 
-		iReport.bDevice = vjoy_device_id;
-		iReport.wAxisX = (val / 128.0f) * max_value;
+	for (int i = 0; i < 5; ++i) fake_midi_devices.push_back(std::string("MIDI ") + std::to_string(i));
+	static int selected_midi_id = 0;
+	ImGui::Text("MIDI Devices");
 
-		UpdateVJD(vjoy_device_id, (PVOID)(&iReport));
-	}
+	ImVec2 region = ImGui::GetContentRegionAvail();
+	ImGui::PushItemWidth(region.x);
 
-}
+	if (ImGui::BeginCombo("##combo_MIDIDevices", fake_midi_devices[selected_midi_id].c_str())) {
 
-void clear_console() {
-	system("CLS");
-	printf("Vendor: %S\nProduct :%S\nVersion Number:%S\n", \
-		TEXT(GetvJoyManufacturerString()), \
-		TEXT(GetvJoyProductString()), \
-		TEXT(GetvJoySerialNumberString()));
-	printf("Acquired vJoy Device: %s \t Selected MIDI Device: %s\n", std::to_string(vjoy_device_id).c_str(), std::to_string(midi_device_id).c_str());
-}
-
-void show_existing_vjoy_devices() {
-	printf("\n\nExisting vJoy Devices (ID value): [ ");
-	for (int i = 0; i < 16; ++i) {
-		if (isVJDExists(i)) {
-			printf((std::string("\t") + std::to_string(i)).c_str());
+		for (int i = 0; i < 5; ++i) {
+			bool is_selected = selected_midi_id == i;
+			if (ImGui::Selectable(fake_midi_devices[i].c_str(), is_selected)) {
+				selected_midi_id = i;
+				break;
+			}
 		}
+		ImGui::EndCombo();
 	}
-	printf("\t]\n\n");
+
+	ImGui::PopItemWidth();
 }
 
-void show_existing_midi_devices(RtMidiIn* midi_in) {
-	printf("\n\nExisting MIDI Devices (ID: Name): \n");
-	//unsigned int midi_device_count = midi_in->getPortCount();
-	for (unsigned int i = 0; i < midi_in->getPortCount(); ++i) {
-		std::string midi_device_name = midi_in->getPortName(i);
-		printf((std::to_string(i) + std::string("\t") + midi_device_name + std::string("\n")).c_str());
+void gui_devices_vjoy() {
+	std::vector<std::string> fake_vjoy_devices;
+
+	for (int i = 0; i < 5; ++i) fake_vjoy_devices.push_back(std::string("VJOY ") + std::to_string(i + 1));
+
+	static int selected_vjoy_id = 0;
+	ImGui::Text("vJoy Devices");
+
+	ImVec2 region = ImGui::GetContentRegionAvail();
+	ImGui::PushItemWidth(region.x);
+
+	if (ImGui::BeginCombo("##combo_vJoyDevices", fake_vjoy_devices[selected_vjoy_id].c_str())) {
+
+		for (int i = 0; i < 5; ++i) {
+			bool is_selected = selected_vjoy_id == i;
+			if (ImGui::Selectable(fake_vjoy_devices[i].c_str(), is_selected)) {
+				selected_vjoy_id = i;
+				break;
+			}
+		}
+		ImGui::EndCombo();
 	}
-	printf("\n");
+
+	ImGui::PopItemWidth();
 }
 
-int user_select_vjoy_device() {
-	show_existing_vjoy_devices();
-	printf("Choose a vJoy Device\n");
+void gui_device_manager() {
+	ImGui::Begin("Device Manager", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize);
 
-	int vjoy_select_input;
-	std::cin >> vjoy_select_input;
-	if (isVJDExists(vjoy_select_input) && GetVJDStatus(vjoy_select_input) == VJD_STAT_FREE) {
-		vjoy_device_id = vjoy_select_input;
-		clear_console();
-		return 0;
-	}
-	else {
-		printf("vJoy Device #%d is disabled.\n", vjoy_select_input);
-		return -1;
-	}
+	gui_devices_midi();
+	gui_devices_vjoy();
+
+	ImGui::End();
 }
 
-int user_select_midi_device(RtMidiIn* midi_in) {
-	show_existing_vjoy_devices();
-	printf("Choose a MIDI Device\n");
-
-	int midi_select_input;
-	std::cin >> midi_select_input;
-	if (midi_select_input >= 0 && midi_select_input < midi_in->getPortCount()) {
-		midi_device_id = midi_select_input;
-		clear_console();
-		return 0;
+void gui_midi_monitor() {
+	ImGui::Begin("MIDI Monitor", nullptr, ImGuiWindowFlags_NoMove);
+	for (int i = 0; i < 10; ++i) {
+		ImGui::Text("Channel: %d\tCC: %d\tValue:%d", 1, i + 25, i * 10);
 	}
-	else {
-		printf("That's not a valid MIDI device\n");
-		return -1;
-	}
+	ImGui::End();
 }
+
+#pragma endregion
+
 
 int main() {
-	
-	// Setup midi device
-	RtMidiIn* midi_in = new RtMidiIn();
-	if (midi_in->getPortCount() == 0) {
-		printf("No MIDI devices available.\n\n");
-		return -1;
-	}
-	else {
-		std::string name = midi_in->getPortName(midi_device_id);
-		printf("Found device: %s\n\n", name.c_str());
 
-		midi_in->openPort(midi_device_id);
-		midi_in->setCallback(&midi_monitor_callback);
-	}
+#pragma region Window/OpenGL Setup
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Check that vJoy is running
-	if (!vJoyEnabled()) {
-		printf("vJoy is disabled. Make sure it's running and click \"Enable vJoy\"\n\n");
-		return -1;
-	}
-	else {
-		clear_console();
-	}
+	GLFWwindow* window = glfwCreateWindow(800, 600, (APP_TITLE + std::string("\tv") + APP_VERSION).c_str(), nullptr, nullptr);
+	glfwMakeContextCurrent(window);
 
-	// Acquire vJoy device
-	if (!AcquireVJD(vjoy_device_id)) {
-		printf("Failed to acquire vJoy Device #%d\n\n", vjoy_device_id);
-		return -1;
-	}
-	else {
-		printf("Acquired vJoy Device #%d\n\n", vjoy_device_id);
-	}
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+#pragma endregion
 
-	// Check what axes are used with this vJoy device
-	cache_used_axes();
+#pragma region GLFW Callbacks
+	glfwSetFramebufferSizeCallback(window, glfw_framebuffer_size_callback);
+#pragma endregion
 
-UserInput:
-	printf(
-		"\n#\tFunction\n1\t%s\n2\t%s\nAwaiting input ... ",
-		"Select vJoy Device",
-		"Select MIDI Device"
-	);
-	char input;
-	std::cin >> input;
+#pragma region ImGui Setup
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Control
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 
-	clear_console();
-	switch (input) {
-	case '1':
+	ImGui::StyleColorsDark();
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.WindowRounding = 0.0f;
+	style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(GLSL_VERSION);
+#pragma endregion
+
+#pragma region Window Loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		imgui_new_frame();
 		
-		while (user_select_vjoy_device() == -1) {}
+		ImGuiID dockspace_id = ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+		gui_dock_initial_layout(window,dockspace_id);
 
-		goto UserInput;
-		break;
-	case '2':
-		show_existing_midi_devices(midi_in);
+		gui_input_binder();
+		gui_device_manager();
+		//gui_devices_midi();
+		//gui_devices_vjoy();
+		gui_midi_monitor();
 
-		while (user_select_midi_device(midi_in) == -1) {}
+		ImGui::Render();
 
-		goto UserInput;
-		break;
-	default:
-		printf("Good bye");
-		goto Cleanup;
-		break;
+		glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// Let ImGui update viewports
+		GLFWwindow* backup_context = glfwGetCurrentContext();
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+		glfwMakeContextCurrent(backup_context);
+
+		glfwSwapBuffers(window);
 	}
-	
+#pragma endregion
 
-	// Cleanup 
-Cleanup:
-	RelinquishVJD(vjoy_device_id);
-	delete midi_in;
+#pragma region Cleanup
 
-	// Get outta heaaaa!
+#pragma endregion
+
 	return 0;
 }
+
+
+
+
+//#include "nlohmann/json.hpp"
+//#include <fstream>
+//
+//using JSON = nlohmann::json;
+//JSON file_to_json(const char* path) {
+//	std::ifstream i(path);
+//	JSON j;
+//	i >> j;
+//	return j;
+//}
+//
+//const char* config_path = "C:/Users/brand/Downloads/config.json";
+//
+//int main() {
+//	JSON j = file_to_json(config_path);
+//
+//
+//
+//	return 0;
+//}
+
+//#include "AppHeaders.h"
+//#include <unordered_map>
+//
+//enum eAxis {
+//	X, Y, Z, RX, RY, RZ, SL0, SL1
+//};
+//
+//struct MIDI_CC {
+//	int channel;
+//	int id;
+//
+//	MIDI_CC() {
+//		channel = 1;
+//		id = 0;
+//	}
+//
+//	MIDI_CC(int midi_channel, int cc) {
+//		channel = midi_channel;
+//		id = cc;
+//	}
+//};
+//
+//struct VJOYMIDI_BINDING {
+//	int device_id;
+//	MIDI_CC axes[8];
+//};
+//
+//int vjoy_device_id = 1;
+//int midi_device_id = 0;
+//const int MIDI_MSG_CC = 176;
+//const long max_value = 0x8000;
+//
+//BOOL used_axis_cache[8];
+//
+//JOYSTICK_POSITION_V2 iReport;
+//VJOYMIDI_BINDING binding;
+//eAxis axis_bind_type = eAxis::X;
+//
+//MIDI_CC last_midi_message;
+//
+//void cache_used_axes() {
+//	used_axis_cache[eAxis::X] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_X);
+//	used_axis_cache[eAxis::Y] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_Y);
+//	used_axis_cache[eAxis::Z] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_Z);
+//	used_axis_cache[eAxis::RX] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_RX);
+//	used_axis_cache[eAxis::RY] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_RY);
+//	used_axis_cache[eAxis::RZ] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_RZ);
+//	used_axis_cache[eAxis::SL0] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_SL0);
+//	used_axis_cache[eAxis::SL1] = GetVJDAxisExist(vjoy_device_id, HID_USAGE_SL1);
+//}
+//
+//void midi_bind_callback(double deltatime, std::vector<unsigned char>* message, void* userData) {
+//	int status = (int)message->at(0);
+//	if (status >= MIDI_MSG_CC && status < MIDI_MSG_CC + 16) {
+//		int channel = status - MIDI_MSG_CC + 1;
+//		int cc = (int)message->at(1);
+//
+//		MIDI_CC axis = binding.axes[axis_bind_type];
+//		axis.channel = channel;
+//		axis.id = cc;
+//	}
+//}
+//
+//void midi_monitor_callback(double deltatime, std::vector< unsigned char > *message, void *userData)
+//{
+//	int status = (int)message->at(0);
+//	if (status >= MIDI_MSG_CC && status < MIDI_MSG_CC + 16) {
+//		int channel = status - MIDI_MSG_CC + 1;
+//		int cc = (int)message->at(1);
+//		int val = (int)message->at(2);
+//
+//		iReport.bDevice = vjoy_device_id;
+//		iReport.wAxisX = (val / 128.0f) * max_value;
+//
+//		UpdateVJD(vjoy_device_id, (PVOID)(&iReport));
+//	}
+//
+//}
+//
+//void clear_console() {
+//	system("CLS");
+//	printf("Vendor: %S\nProduct :%S\nVersion Number:%S\n", \
+//		TEXT(GetvJoyManufacturerString()), \
+//		TEXT(GetvJoyProductString()), \
+//		TEXT(GetvJoySerialNumberString()));
+//	printf("Acquired vJoy Device: %s \t Selected MIDI Device: %s\n", std::to_string(vjoy_device_id).c_str(), std::to_string(midi_device_id).c_str());
+//}
+//
+//void show_existing_vjoy_devices() {
+//	printf("\n\nExisting vJoy Devices (ID value): [ ");
+//	for (int i = 0; i < 16; ++i) {
+//		if (isVJDExists(i)) {
+//			printf((std::string("\t") + std::to_string(i)).c_str());
+//		}
+//	}
+//	printf("\t]\n\n");
+//}
+//
+//void show_existing_midi_devices(RtMidiIn* midi_in) {
+//	printf("\n\nExisting MIDI Devices (ID: Name): \n");
+//	//unsigned int midi_device_count = midi_in->getPortCount();
+//	for (unsigned int i = 0; i < midi_in->getPortCount(); ++i) {
+//		std::string midi_device_name = midi_in->getPortName(i);
+//		printf((std::to_string(i) + std::string("\t") + midi_device_name + std::string("\n")).c_str());
+//	}
+//	printf("\n");
+//}
+//
+//int user_select_vjoy_device() {
+//	show_existing_vjoy_devices();
+//	printf("Choose a vJoy Device\n");
+//
+//	int vjoy_select_input;
+//	std::cin >> vjoy_select_input;
+//	if (isVJDExists(vjoy_select_input) && GetVJDStatus(vjoy_select_input) == VJD_STAT_FREE) {
+//		vjoy_device_id = vjoy_select_input;
+//		clear_console();
+//		return 0;
+//	}
+//	else {
+//		printf("vJoy Device #%d is disabled.\n", vjoy_select_input);
+//		return -1;
+//	}
+//}
+//
+//int user_select_midi_device(RtMidiIn* midi_in) {
+//	show_existing_vjoy_devices();
+//	printf("Choose a MIDI Device\n");
+//
+//	int midi_select_input;
+//	std::cin >> midi_select_input;
+//	if (midi_select_input >= 0 && midi_select_input < midi_in->getPortCount()) {
+//		midi_device_id = midi_select_input;
+//		clear_console();
+//		return 0;
+//	}
+//	else {
+//		printf("That's not a valid MIDI device\n");
+//		return -1;
+//	}
+//}
+//
+//int main() {
+//	
+//	// Setup midi device
+//	RtMidiIn* midi_in = new RtMidiIn();
+//	if (midi_in->getPortCount() == 0) {
+//		printf("No MIDI devices available.\n\n");
+//		return -1;
+//	}
+//	else {
+//		std::string name = midi_in->getPortName(midi_device_id);
+//		printf("Found device: %s\n\n", name.c_str());
+//
+//		midi_in->openPort(midi_device_id);
+//		midi_in->setCallback(&midi_monitor_callback);
+//	}
+//
+//	// Check that vJoy is running
+//	if (!vJoyEnabled()) {
+//		printf("vJoy is disabled. Make sure it's running and click \"Enable vJoy\"\n\n");
+//		return -1;
+//	}
+//	else {
+//		clear_console();
+//	}
+//
+//	// Acquire vJoy device
+//	if (!AcquireVJD(vjoy_device_id)) {
+//		printf("Failed to acquire vJoy Device #%d\n\n", vjoy_device_id);
+//		return -1;
+//	}
+//	else {
+//		printf("Acquired vJoy Device #%d\n\n", vjoy_device_id);
+//	}
+//
+//	// Check what axes are used with this vJoy device
+//	cache_used_axes();
+//
+//UserInput:
+//	printf(
+//		"\n#\tFunction\n1\t%s\n2\t%s\nAwaiting input ... ",
+//		"Select vJoy Device",
+//		"Select MIDI Device"
+//	);
+//	char input;
+//	std::cin >> input;
+//
+//	clear_console();
+//	switch (input) {
+//	case '1':
+//		
+//		while (user_select_vjoy_device() == -1) {}
+//
+//		goto UserInput;
+//		break;
+//	case '2':
+//		show_existing_midi_devices(midi_in);
+//
+//		while (user_select_midi_device(midi_in) == -1) {}
+//
+//		goto UserInput;
+//		break;
+//	default:
+//		printf("Good bye");
+//		goto Cleanup;
+//		break;
+//	}
+//	
+//
+//	// Cleanup 
+//Cleanup:
+//	RelinquishVJD(vjoy_device_id);
+//	delete midi_in;
+//
+//	// Get outta heaaaa!
+//	return 0;
+//}
